286241fe8a2d9a239ae5e4db18fbb7f9b5fc07e9
Grace <you@example.com>
2021-09-28 17:08:52 +0000

    JUST STARTED

---
+++ tendo-clone/src/App.js
@@ -0,0 +1,68 @@
+function App() {
+  return (
+    <div className="App"> 
+    <nav>
+
+    <img src="" alt="" className="logo"/>  
+
+      <div className="nav-right">
+        <ul>
+          <li>Home</li>
+          <li>Become a Supplier</li>
+          <li>Get App</li>
+
+        </ul>
+      </div>
+  </nav>
+  
+  <header>
+    <div className="header-left">
+    </div>
+    
+    <img src="" alt="" className="header-right"/>
+    </header>
+
+    </div>
+  );
+
+}
+
+
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
