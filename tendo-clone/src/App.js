a4d8c46513618410965f4deb6d20ea9526e177a2
Grace <you@example.com>
2021-09-30 23:16:45 +0000

    Styling

--- tendo-clone/src/App.js
+++ tendo-clone/src/App.js
@@ -1,33 +1,35 @@
+import  "./App.css"
+
 function App() {
   return (
     <div className="App"> 
-    <nav>
 
-    <img src="" alt="" className="logo"/>  
+  <nav className="navbar">
+    <h1 className="logo">tendo</h1>
+    <ul className="nav-links">
+      <li><a href="/">Home</a></li>
+      <li><a href="/">Become a supplier </a></li>
+      <li><a href="/" className="btn-primary">Get App</a></li>
+    </ul>
+     </nav>
 
-      <div className="nav-right">
-        <ul>
-          <li>Home</li>
-          <li>Become a Supplier</li>
-          <li>Get App</li>
-
-        </ul>
-      </div>
-  </nav>
-  
-  <header>
+  <header className="header">
     <div className="header-left">
+      <h1>Start your online business from home</h1>
+      <p className="p-start">Tendo enables anyone to start their online business without investing any capital.</p>
+      <p className="p-merchant">Become a Tendo merchant now.</p>
+    <button className="cta-button">Join Tendo</button>
     </div>
-    
-    <img src="" alt="" className="header-right"/>
+    <div className="header-right">
+    <img src="./image/home.png" alt="" srcset="" />
+    </div>
+
     </header>
 
     </div>
   );
 
 }
-
-
 
 export default App;
 
@@ -41,28 +43,3 @@ export default App;
 
 
 
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
